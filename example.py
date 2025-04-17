6403ee2d2cdb9df7c60fc970c1a5428ac5c398f8
manvendra-nema <53614640+manvendra-nema@users.noreply.github.com>
2025-04-17 21:12:24 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+https://colab.research.google.com/drive/1TiiWSfxQhN8jhVW4eaMmyVUIdbp2UeB6#scrollTo=JCCCPqV-RXmc
 def git_opeation():
 
  print("I am adding example.py file to the remote repository.")
